bc66cd0435cc1b95895bc7d2830824af88b67d1e
Prakhar Tripathi <prakhartripathi@Prakhars-MacBook-Pro.local>
2019-08-18 01:03:52 +0530

    added recyclerview with dummy data

--- app/src/main/java/com/example/android104/DataActivity.kt
+++ app/src/main/java/com/example/android104/DataActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_data.*
+import java.util.ArrayList
 
 class DataActivity : AppCompatActivity() {
 
-    lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: RecyclerAdapter
+    private var photosList: ArrayList<UserData> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +18,24 @@ class DataActivity : AppCompatActivity() {
 
         linearLayoutManager = LinearLayoutManager(this)
         my_recycler_view.layoutManager = linearLayoutManager
+
+        adapter = RecyclerAdapter(photosList)
+        my_recycler_view.adapter = adapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (photosList.size == 0) {
+            val user1 = UserData("Prakhar", "123", "123")
+            val user2 = UserData("Prakhar 2", "123", "123")
+            val user3 = UserData("Prakhar 3", "123", "123")
+            photosList.add(user1)
+            adapter.notifyItemInserted(0)
+            photosList.add(user2)
+            adapter.notifyItemInserted(1)
+            photosList.add(user3)
+            adapter.notifyItemInserted(2)
+        }
     }
 }
---
+++ app/src/main/java/com/example/android104/RecyclerAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.android104
+
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclerview_row.view.*
+
+class RecyclerAdapter(private val userdata: ArrayList<UserData>) : RecyclerView.Adapter<RecyclerAdapter.UserDataHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserDataHolder {
+        val inflatedView = parent.inflate(R.layout.recyclerview_row, false)
+        return UserDataHolder(inflatedView)
+    }
+
+    override fun getItemCount(): Int {
+        return userdata.size
+    }
+
+    override fun onBindViewHolder(holder: UserDataHolder, position: Int) {
+        val data = userdata[position]
+        holder.bindUserData(data)
+    }
+
+    class UserDataHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        private var view: View = v
+        private var userData: UserData? = null
+
+        init {
+            v.setOnClickListener(this)
+        }
+
+        fun bindUserData(data: UserData) {
+            this.userData = data
+            view.textViewName.text = data.name
+            view.textViewNumber.text = data.phone
+            view.textViewEmail.text = data.email
+        }
+
+        override fun onClick(v: View) {
+            Log.d("RecyclerView", "CLICK!")
+        }
+    }
+}
+
+data class UserData(val name: String, val phone: String, val email: String)
