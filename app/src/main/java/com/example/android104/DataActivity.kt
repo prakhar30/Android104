339283797e62eed828fd04dded29afea433fd93b
Prakhar Tripathi <prakhartripathi@Prakhars-MacBook-Pro.local>
2019-08-17 21:49:07 +0530

    added boilerplate for recycler view

--- app/src/main/java/com/example/android104/DataActivity.kt
+++ app/src/main/java/com/example/android104/DataActivity.kt
@@ -2,11 +2,18 @@ package com.example.android104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_data.*
 
 class DataActivity : AppCompatActivity() {
+
+    lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data)
+
+        linearLayoutManager = LinearLayoutManager(this)
+        my_recycler_view.layoutManager = linearLayoutManager
     }
 }
